70a78db5790a1f3910057d7308b3f258e9023b3f
BubbaTam <jamesglassey@outlook.com>
2022-07-27 08:07:58 +0100

    feat: add config locations

---
+++ config.py
@@ -0,0 +1,4 @@
+PROCESSED_DATA_FOLDER = r"data_sources\processed_data"
+RAW_DATA_FILE = r"data_sources\raw_data\export.xml"
+RAW_DATA_FOLDER = r"data_sources\raw_data"
+MUSIC_JSON_FILE = r"data_sources\raw_data\today_music.json"
